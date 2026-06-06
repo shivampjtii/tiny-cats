f42c8e68ecba0def75701f7543b1411854f21af2
shivampjtii <shivampjtii@gmail.com>
2026-06-06 21:18:59 +0530

    config type

--- Backend/src/config/config.ts
+++ Backend/src/config/config.ts
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
+import type { IConfig } from "../types/config.type.ts";
 dotenv.config();
 
-const config = {
-    PORT: process.env.PORT,
-    DB_URI: process.env.DB_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-}
+// interface IConfig {
+//   PORT: string | undefined;
+//   MONGO_URI: string | undefined;
+//   JWT_SECRET: string | undefined;
+// }
+
+const config: IConfig = {
+  PORT: process.env.PORT,
+  MONGO_URI: process.env.MONGO_URI,
+  JWT_SECRET: process.env.JWT_SECRET,
+};
 
 export default config;
---
+++ Backend/src/types/config.type.ts
@@ -0,0 +1,5 @@
+export interface IConfig {
+  PORT: string | undefined;
+  MONGO_URI: string | undefined;
+  JWT_SECRET: string | undefined;
+}
